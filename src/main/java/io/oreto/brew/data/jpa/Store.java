a57988fe4fbaa3d760fd828a3ad6ae2050b6e32e
ross-oreto <ross.oreto@gmail.com>
2020-11-25 17:45:41 -0600

    add default methods for store interface

--- src/main/java/io/oreto/brew/data/jpa/Store.java
+++ src/main/java/io/oreto/brew/data/jpa/Store.java
@@ -3,9 +3,38 @@ package io.oreto.brew.data.jpa;
 import io.oreto.brew.data.Crud;
 import io.oreto.brew.data.Paged;
 
+import javax.persistence.EntityManager;
 import java.util.Optional;
 
 public interface Store<ID, T> extends Crud<ID, T> {
-    Paged<T> FindAll(String q, Paged.Page page, String... fetch);
-    Optional<T> FindOne(String q, Paged.Page page, String... fetch);
+    EntityManager getEntityManager();
+    Class<T> getEntityClass();
+
+    default Paged<T> FindAll(String q, Paged.Page page, String... fetch) {
+        return DataStore.findAll(getEntityManager(), getEntityClass(), q, page, fetch);
+    }
+
+    default Optional<T> FindOne(String q, Paged.Page page, String... fetch) {
+        return DataStore.findOne(getEntityManager(), getEntityClass(), q, page, fetch);
+    }
+
+    @Override
+    default T Create(T t, String... fetch) {
+        return DataStore.save(getEntityManager(), t, fetch);
+    }
+
+    @Override
+    default Optional<T> Retrieve(ID id, String... fetch) {
+       return DataStore.get(getEntityManager(), getEntityClass(), id, fetch);
+    }
+
+    @Override
+    default T Update(T t, String... fetch) {
+        return DataStore.update(getEntityManager(), t, fetch);
+    }
+
+    @Override
+    default T Delete(ID id) {
+        return DataStore.delete(getEntityManager(), getEntityClass(), id);
+    }
 }
